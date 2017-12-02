d584840b4b37cd0387176b6fe7a8c8395170e0cf
Henrik Numé <henriknume@gmail.com>
2017-12-02 14:38:00 +0100

    day2 done.

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,28 @@
+def checksum(inp):
+    rows = inp.splitlines()
+    s = 0
+    for r in rows:
+        numbers = map(int, r.split())
+        s += max(numbers) - min(numbers)
+    return s
 
-def foo():
-    return 0
+
+def divs(inp):
+    rows = inp.splitlines()
+    s = 0
+    for r in rows:
+        numbers = map(int, r.split())
+        for n in numbers:
+            for d in numbers:
+                if (n % d) == 0 and n != d:
+                    s += (n / d)
+    return s
+
 
 def main():
-    inp = open("./../inputs/d2.txt").read()
-    print "Part 1: %d" % foo()
-    print "Part 2: %d" % foo()
+    inp = open("./d2.txt").read()
+    print "Part 1: %d" % checksum(inp)
+    print "Part 2: %d" % divs(inp)
 
 
 if __name__ == "__main__": main()
